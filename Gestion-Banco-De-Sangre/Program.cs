0853e4659aac044f20c096b299425d292d6ccbe0
Josafat <53987304+JosafatZG@users.noreply.github.com>
2021-12-22 21:39:43 -0600

    .gitignore

--- Gestion-Banco-De-Sangre/Program.cs
+++ Gestion-Banco-De-Sangre/Program.cs
@@ -16,7 +16,7 @@ namespace Gestion_Banco_De_Sangre
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new GestionSangre());
         }
     }
 }
